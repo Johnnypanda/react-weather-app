1e2c720831eb5b21771aeb3463c7d9df14afb791
unknown <afilinov93@gmail.com>
2018-06-26 12:50:01 +0200

    add API data to the actions

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+//API key might be obsolete, make sure it's fine or replace it
+import axios from 'axios';
+
+const API_KEY = '12d10aff5c29b4947737f39b00533f83';
+const ROOT_URL = `https://openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
