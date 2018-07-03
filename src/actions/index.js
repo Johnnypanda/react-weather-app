992094c0a470f1d3a364bf3a6ac0915e27f0286b
Johnnypanda <afilinov93@gmail.com>
2018-07-03 14:52:43 +0200

    fix api key, configure sparklines

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,8 @@
 //Axios fetches an api and returns a url, just like jquery GET method
 import axios from 'axios';
 
-const API_KEY = '12d10aff5c29b4947737f39b00533f83';
-const ROOT_URL = `https://openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const API_KEY = '12d10aff5c29b4947737f39b00533f83'; 
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
